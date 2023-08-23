0e0050bfa9fad312b7cc21e2a1cd08453a4cb8c1
suriyasingaravel <suriyasingaravel710.com>
2023-08-23 23:29:54 +0530

    day-1

---
+++ ossified-celery-3594/src/App.jsx
@@ -0,0 +1,15 @@
+// import AllRoutes from "./Components/AllRoutes"
+import Navbar from "./Components/Navbar"
+
+
+function App() {
+  return (
+    <>
+   <Navbar />
+   {/* <AllRoutes /> */}
+    </>
+
+  )
+}
+
+export default App

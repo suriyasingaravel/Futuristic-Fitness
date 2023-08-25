d8597f4653506680ff1434562ee71b84cefd4b86
suriyasingaravel <suriyasingaravel710.com>
2023-08-26 00:51:54 +0530

    day-04

--- ossified-celery-3594/src/Components/AllRoutes.jsx
+++ ossified-celery-3594/src/Components/AllRoutes.jsx
@@ -2,6 +2,7 @@ import {Routes,Route} from "react-router-dom"
 import Home from "../pages/Home"
 import Destination from "../pages/Destination"
 import About from "../pages/About"
+import Login from "../pages/Login"
 function AllRoutes(){
 return (
     <Routes>
@@ -9,7 +10,8 @@ return (
         <Route path="/explore" element={<Destination />} />
         <Route path="/about" element={<About />} />
         <Route path="/explore" element={<Destination />} />
-</Routes>
+        <Route path="/login" element={<Login />} />
+   </Routes>
 )
 }
 export default AllRoutes
---
+++ ossified-celery-3594/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import BMICalculator from '../Components/BMICalculator'
+
+const Login = () => {
+  return (
+      <div style={{paddingTop:"100px", backgroundColor:"black", color:"whitesmoke"}}>
+      <BMICalculator/>
+      </div>
+   
+    
+  )
+}
+
+export default Login
